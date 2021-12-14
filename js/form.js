c2b533ed98cb6ecb2da2afc480bcdfe8757f965c
Lục Thiên Phong <90561566+lucthienphong1120@users.noreply.github.com>
2021-12-14 22:31:01 +0700

    Update form.js

--- js/form.js
+++ js/form.js
@@ -9,9 +9,9 @@ function sendmail() {
    var Body = 'Tên: ' + name + '<br>Email: ' + email + '<br>Chủ đề: ' + subject + '<br>Nội dung: ' + message;
    console.log(name, email, subject, message);
    Email.send({
-      SecureToken: "c4ae55fc-10a8-4e51-91e7-6da154b10ea5",
-      To: 'aichoibangbangko@gmail.com',
-      From: "aichoibangbangko@gmail.com",
+      SecureToken: "6c48748a-5544-45f3-94f2-d42e4cf07810",
+      To: 'chung8962003@gmail.com',
+      From : "aichoibangbangko@gmail.com",
       Subject: "Tin nhắn mới từ " + name,
       Body: Body
    }).then(
