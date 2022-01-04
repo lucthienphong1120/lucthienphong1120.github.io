8e6fc640d65238473be6703e9ad50cb6de15da00
admin <chung.bkc13061@student.bkacad.edu.vn>
2022-01-04 10:14:33 +0700

    update protect source

--- js/menu.js
+++ js/menu.js
@@ -17,9 +17,21 @@ var announcement = document.getElementById("announcement");
 var announcementContent = document.getElementById("announcement__content");
 var okBtn = document.getElementById("announcement__ok");
 var closeBtn = document.getElementById("announcement__close");
+var typeTitle = document.querySelector('.type-title');
+var typeName = document.querySelector('.type-name');
+
+if(document.body.clientWidth <= 400) {
+    announcement.style.display = "none";
+	typeTitle.style.animation = "type5 1.5s steps(40,end) 1.5s forwards";
+	typeName.style.animation = "shring 2s steps(40,end) 2s forwards, blink5 .5s step-end infinite alternate";
+	document.body.classList.remove('disable-scroll');
+}
 
 function hide() {
     announcement.style.display = "none";
+	typeTitle.style.animation = "type5 1.5s steps(40,end) forwards";
+	typeName.style.animation = "shring 2s steps(40,end) forwards, blink5 .5s step-end infinite alternate";
+	document.body.classList.remove('disable-scroll');
 }
 
 closeBtn.addEventListener("click", hide);
